db4b5a985262e3e11b2fb23a0e8bdede7507d119
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-04-06 16:56:11 -0400

    game board working a little

--- triviaAPI.js
+++ triviaAPI.js
@@ -1,11 +1,31 @@
 let questionArray = [];
 let answerArray = [];
 let imdbID = "tt";
+let topicArray = ["Movie","Star Wars","Trivia"]
+let randomTopic = Math.random();
+
+if (randomTopic < .5)
+{
+    randomTopic = 1;
+}
+else
+{
+    randomTopic = 2;
+}
+
+let currentAnswerLength = 0;
+let answerBox = "";
+let currentQuestionArray = [];
+let currentAnswerArray = [];
+
+
+
+
 
 let randomMovieNumber = () =>
 {
 
-    let imdbRandom = String(Math.floor(Math.random() * 2404799));
+    let imdbRandom = String(Math.floor(Math.random() * 6999999));
 
     if (imdbRandom.length === 1)
     {
@@ -42,46 +62,80 @@ let randomMovieNumber = () =>
 let runStarWars = () =>
 {
     $.ajax(
-    {   
-        url: "https://swapi.co/api/people?amount=100",
-        type: "GET",
-        data:
+        {   
+            url: "https://swapi.co/api/people?amount=100",
+            type: "GET",
+            data:
+            {
+            "$limit" : 100,
+            //"$$app_token" : ""
+            }
+        }).done(function(data) 
         {
-        "$limit" : 100,
-        //"$$app_token" : ""
-        }
-    }).done(function(data) 
-    {
-        for (let i = 0; i < data.results.length; i++)
-        {
-            let $li = $("<li>").text(data.results[i].name)
-            $("body").append($li);
-            questionArray.push($li);
-            answerArray.push($li);
-        }
-    });
+            let randomStarWars = Math.floor((Math.random() * data.results.length));
+
+            for (let i = 0; i < (data.results[randomStarWars].name.length); i++)
+            {
+                if ((data.results[randomStarWars].name[i] === ".") || (data.results[randomStarWars].name[i] === "'") || (data.results[randomStarWars].name[i] === ";") || (data.results[randomStarWars].name[i] === ":"))
+                {
+                    runStarWars();
+                    return;
+                }
+                else
+                {
+
+                }
+            }
+
+            currentAnswerArray[0] = data.results[randomStarWars].name;
+
+            currentAnswerLength = currentAnswerArray[0].length;
+            updateBoard();
+
+
+        });
+
+        
 }
 
 let runTrivia = () =>
 {
     $.ajax(
         {   
-            url: "https://opentdb.com/api.php?amount=200",
+            url: "https://opentdb.com/api.php?amount=1",
             type: "GET",
             data2:
             {
-            "$limit" : 10,
+            "$limit" : 1,
             "$$app_token" : "https://opentdb.com/api_token.php?command=request"
             }
         }).done(function(data2) 
         {
+            for (let i = 0; i < (data2.results[0].correct_answer.length); i++)
+            {
+                if ((data2.results[0].correct_answer[i] === ".") || (data2.results[0].correct_answer[i] === "'") || (data2.results[0].correct_answer[i] === ";") || (data2.results[0].correct_answer[i] === ":"))
+                {
+                    runTrivia();
+                    return;
+                }
+                else
+                {
+
+                }
+            }
+
             for (let i = 0; i < data2.results.length; i++)
             {
                 let $li2 = $("<li>").text(data2.results[i].question)
+                let $li3 = $("<li>").text(data2.results[i].correct_answer)
                 $("body").append($li2);
-                questionArray.push($li2);
-                answerArray.push($li2);
+                $("body").append($li3);
+                currentQuestionArray[0] = data2.results[i].question;
+                currentAnswerArray[0] = data2.results[i].correct_answer;
             }
+
+            currentAnswerLength = currentAnswerArray[0].length;
+            updateBoard();
         });
 }
 
@@ -90,11 +144,11 @@ let runMovie = () =>
 
     $.ajax(
         {   
-            url: "http://www.omdbapi.com/?apikey=29372440&i=0000001",
+            url: "http://www.omdbapi.com/?apikey=29372440&i=" + imdbID,
             type: "GET",
             data3:
             {
-            "$limit" : 10,
+            "$limit" : 100,
             //"$$app_token" : "29372440"
             }
         }).then(function(data3) 
@@ -102,14 +156,16 @@ let runMovie = () =>
             let director = data3.Director;
             let year = data3.Year;
             let actors = data3.Actors;
-            let rating = data3.Ratings;
+            let rating = Number(data3.imdbRating);
             let title = data3.Title;
             let typeMovie = data3.Type;
-    
-            /*
-            if (typeMovie === "movie")
+            let country = data3.Country;
+            let rated = data3.Rated;
+            
+            
+            if ((typeMovie === "movie") && (country === "USA") && (rating > 7.0) && (rated!== "N/A") && (year > 2000))
             {
-
+            
             }  
             
             else 
@@ -119,20 +175,22 @@ let runMovie = () =>
                 runMovie();
                 return;
             }
-            */
             
             let $li3 = $(`<li>Director:  ${director}</li>`);
             let $li4 = $(`<li>Year:  ${year}</li>`);
             let $li5 = $(`<li>Actors:  ${actors}</li>`);
-            let $li6 = $(`<li>Rating:  ${rating}</li>`);
+            //let $li6 = $(`<li>Rating:  ${rating}</li>`);
             let $li7 = $(`<li>Title:  ${title}</li>`);
             let $li8 = $(`<li>Type of Media:  ${typeMovie}</li>`);
+            let $li9 = $(`<li>Country:  ${country}</li>`);
+            //let $li10 = $(`<li>:  ${}</li>`);
 
             $("body").append($li3);
             $("body").append($li4);
             $("body").append($li5);
-            $("body").append($li6);
+            //$("body").append($li6);
             $("body").append($li8);
+            $("body").append($li9);
 
             $("body").append($li7);
 
@@ -142,12 +200,53 @@ let runMovie = () =>
         })
 }
 
+let updateBoard = () =>
+{
+    let $board = $("<div>").attr("class","board");
+    $("body").append($board);
+    
+    for (let i = 0; i < currentAnswerLength; i++)
+    {
+        if ((currentAnswerArray[0][i]) === " ")
+        {
+            $spaceBox = $("<div>").attr("class","spaceBox");
+            $spaceBox.text(currentAnswerArray[0][i].toUpperCase());
+            $($board).append($spaceBox);
+            
+        }
+        else
+        {
+            $answerBox = $("<div>").attr("class","answerBox");
+            $answerBox.text(currentAnswerArray[0][i].toUpperCase());
+            $($board).append($answerBox);
+        }
+
+    }
+}
+
 $( () =>
 { 
     $("button").on("click", (event) =>
     {
-        randomMovieNumber();
-        runMovie();   
+        debugger;
+                
+        if (randomTopic === 0)
+        {
+            //randomMovieNumber();
+            //runMovie();
+        }
+        else if (randomTopic === 1)
+        {
+            runStarWars();
+        }
+        else if(randomTopic === 2)
+        {
+            runTrivia();
+        }
+        else
+        {
+
+        }
+
     });
 });
-
